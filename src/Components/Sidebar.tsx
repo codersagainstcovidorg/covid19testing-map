9d1feb881e1fdcce9d34cfdc87c493de914e6edb
Jorge Caballero <jorge.caballero@icloud.com>
2020-03-19 07:26:53 -0700

    add vertical stepper to sidebar, remove tab panel

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -3,7 +3,9 @@ import {
     Drawer, Box, Typography, Tabs, Tab, AppBar,
     FormGroup, FormControlLabel, Switch, Divider
 } from '@material-ui/core';
+import { VerticalLinearStepper } from './StepperVertical';
 import { SearchContext, SearchFilters, labelMap } from '../App';
+
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -43,16 +45,25 @@ export const Sidebar = ({ drawerOpen, toggleFilter }: SidebarProps) => {
                     anchor="left"
                     open={drawerOpen}
                 >
-                    <div style={{ paddingTop: '48px' }}>
-                        <AppBar position="static">
+                    <div style={{ paddingTop: '32px', minWidth: '25%', maxWidth: '450px' }}>
+                        {/* <AppBar position="static">
                             <Tabs value={tabIdx} onChange={(e, v) => setTab(v)}>
                                 <Tab label="Filters" />
                                 {0 ? <Tab label="Place" /> : ''}
                             </Tabs>
-                        </AppBar>
+                        </AppBar> */}
 
-                        <TabPanel value={tabIdx} index={0}>
-                            <FormGroup>
+                        {/* <TabPanel value={tabIdx} index={0}> */}
+                            <Typography style={{padding: '12%', fontWeight: 'bold', fontSize: '1.3rem', color: '#E45B26', textAlign: 'center'}}>
+                                If this is a medical emergency, stop and dial 911. 
+                            </Typography>
+                            {/* <Typography style={{padding: '15%', fontWeight: 'bold', fontSize: '1.8rem', color: '#E45B26', alignContent: 'center'}}>
+                                If you are experiencing fever, chills, cough, headache or another flu-like symptom CALL BEFORE you head to any healthcare facility.
+                            </Typography> */}
+                            
+                            <Divider style={{margin: 10}} />
+                            
+                            <FormGroup style={{paddingLeft: '15%', paddingRight: '10%',  paddingTop: '16px', paddingBottom: '16px',}}>
 
                                 {Object.keys(searchFilters).map((filter : string, idx: number) => {
                                     return (<FormControlLabel
@@ -60,22 +71,21 @@ export const Sidebar = ({ drawerOpen, toggleFilter }: SidebarProps) => {
                                         control={<Switch size="small" checked={searchFilters[filter as keyof SearchFilters]}
                                         onChange={() => toggleFilter(filter)} />}
                                         label={labelMap[filter]}
+                                        labelPlacement="start"
                                     />);
                                 })}
 
                             </FormGroup>
 
-
                             <Divider style={{margin: 10}} />
-                            <Typography style={{padding: 10, width: 260}}>
-                                If this is a medical emergency stop and call 911. If you are experiencing fever, chills, cough, headache or another flu-like symptom CALL BEFORE you head to any healthcare facility.
-                            </Typography>
+                            
+                            <VerticalLinearStepper />
 
-                        </TabPanel>
+                        {/* </TabPanel> */}
 
-                        {0 ? <TabPanel value={tabIdx} index={1}>
+                        {/* {0 ? <TabPanel value={tabIdx} index={1}>
 
-                        </TabPanel> : ''}
+                        </TabPanel> : ''} */}
                     </div>
                 </Drawer>
             }
