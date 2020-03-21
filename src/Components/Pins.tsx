7ab3cf3aa37bfd627fac506253ca3c23abd39e08
philiparpin <philip.arpin@me.com>
2020-03-21 16:19:07 -0400

    Added react-ga and View Testing Location Detail Custom Event

--- src/Components/Pins.tsx
+++ src/Components/Pins.tsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Marker } from 'react-map-gl';
 import SvgLocationIcon from "../LocationIcon";
+import ReactGA from 'react-ga';
 
 
 export interface PinsProps {
@@ -13,6 +14,18 @@ const SIZE = 40;
 
 // Important for perf: the markers never change, avoid rerender when the map viewport changes
 export default class Pins extends PureComponent<PinsProps> {
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    }
+
+    handleLinkClicked(locationId: string): void {
+        ReactGA.event({
+            category: 'Location',
+            action: 'View',
+            label: locationId,
+        })
+    }
+
     render() {
         const { data, onClick, onHover } = this.props;
 
@@ -25,7 +38,10 @@ export default class Pins extends PureComponent<PinsProps> {
                             style={{
                                 cursor: "pointer",
                                 transform: `translate(${-SIZE / 2}px,${-SIZE}px)`}}
-                            onClick={() => onClick(place)}
+                            onClick={() => {
+                                onClick(place);
+                                this.handleLinkClicked(place.location_id);
+                            }}
                             onMouseOver={() => onHover(place)}
                         />
                         {/*<LocalHospital viewBox="0 0 24 24" style={{ fontSize: '11px' }} />*/}
