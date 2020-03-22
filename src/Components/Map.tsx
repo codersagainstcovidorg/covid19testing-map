ac5d36f6d863d9045a0a083636b41d4a449e1b8d
Adam Fahy <afahy@stocktwits.com>
2020-03-22 17:07:58 -0400

    Cleanup on Map changes for adding Geocoder

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,15 +1,13 @@
-import React, { useContext } from "react";
 import styled from "styled-components";
-import ReactMapGL, { StaticMap, GeolocateControl, NavigationControl } from "react-map-gl";
+import React, { useContext } from "react";
+import ReactMapGL, { GeolocateControl, NavigationControl } from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 import Pins from "./Pins";
-import {SearchContext, SearchFilters} from "../App";
+import { SearchContext } from "../App";
 import { MAPBOX_TOKEN } from "../constants";
 import { getFilteredPins } from "../utils/getFilteredPins";
-import Geocoder from "react-map-gl-geocoder";
-import InteractiveMap from "react-map-gl";
-import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
-import { DeckGL } from "@deck.gl/react";
 
 type MapProps = {
   onClickPin: Function;
@@ -30,70 +28,73 @@ const Navigation = styled.div`
   position: fixed;
   z-index: 1;
 `;
-    const myMap = React.createRef<InteractiveMap>();
+
+const mapRef = React.createRef<ReactMapGL>();
 
 export const Map = (props: MapProps) => {
-    const {viewState, setViewState, onClickPin } = props;
-    const searchFilters = useContext(SearchContext);
-    const filteredPins = getFilteredPins(searchFilters);
+  const { viewState, setViewState, onClickPin } = props;
+  const searchFilters = useContext(SearchContext);
+  const filteredPins = getFilteredPins(searchFilters);
 
-
-
-
-        return (
-            <div>
-              <ReactMapGL
-                  viewState={viewState}
-                  getCursor={() => "cursor"}
-                  onViewStateChange={setViewState}
-                  ref={myMap}
-                  width="100vw"
-                  height="100vh"
-                  style={{ zIndex: 50 }}
-                  mapStyle="mapbox://styles/mapbox/streets-v11"
-                  mapboxApiAccessToken={MAPBOX_TOKEN}
-              >
-
-
-                <Navigation>
-                    <Geocoder onViewportChange={(data: any) => {
-                        console.log(data);
-                        let { longitude, latitude, zoom } = data;
-                        if (zoom > 10) {
-                            zoom = 10;
-                        }
-                        setViewState({
-                            viewState: { ...viewState, longitude, latitude, zoom }
-                        });
-                    }} position={"top-left"} countries={"US"} mapRef={myMap} mapboxApiAccessToken={MAPBOX_TOKEN}/>
-                  <Zoom>
-                    <NavigationControl
-                        showCompass={false}
-                        captureClick
-                        captureDoubleClick
-                        onViewportChange={(data: any) => {
-                          const { zoom } = data;
-                          setViewState({
-                            viewState: { ...viewState, zoom }
-                          });
-                        }}
-                    />
-                  </Zoom>
-                  <Geolocation>
-                    <GeolocateControl
-                        label="Use my location"
-                        positionOptions={{ enableHighAccuracy: true }}
-                        onGeolocate={(data: any) => {
-                          const { latitude, longitude } = data.coords;
-                          setViewState({
-                            viewState: { ...viewState, latitude, longitude }
-                          });
-                        }}
-                    />
-                  </Geolocation>
-                </Navigation>
-                <Pins data={filteredPins} onClick={onClickPin} onHover={() => {}} />
-              </ReactMapGL>
-            </div>
-        );
-    }
+  return (
+    <div>
+      <ReactMapGL
+        viewState={viewState}
+        getCursor={() => "cursor"}
+        onViewStateChange={setViewState}
+        ref={mapRef}
+        width="100vw"
+        height="100vh"
+        style={{ zIndex: 50 }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Navigation>
+          <Geocoder
+            onViewportChange={(data: any) => {
+              const { longitude, latitude, zoom } = data;
+              setViewState({
+                viewState: {
+                  ...viewState,
+                  longitude,
+                  latitude,
+                  zoom: Math.min(10, zoom)
+                }
+              });
+            }}
+            position="top-left"
+            countries="US"
+            mapRef={mapRef}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+          <Zoom>
+            <NavigationControl
+              showCompass={false}
+              captureClick
+              captureDoubleClick
+              onViewportChange={(data: any) => {
+                const { zoom } = data;
+                setViewState({
+                  viewState: { ...viewState, zoom }
+                });
+              }}
+            />
+          </Zoom>
+          <Geolocation>
+            <GeolocateControl
+              label="Use my location"
+              positionOptions={{ enableHighAccuracy: true }}
+              onGeolocate={(data: any) => {
+                const { latitude, longitude } = data.coords;
+                setViewState({
+                  viewState: { ...viewState, latitude, longitude }
+                });
+              }}
+            />
+          </Geolocation>
+        </Navigation>
+        <Pins data={filteredPins} onClick={onClickPin} onHover={() => {}} />
+      </ReactMapGL>
+    </div>
+  );
+};
