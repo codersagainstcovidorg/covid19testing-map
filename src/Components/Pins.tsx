2594821dd0e67a792bf6c73331c8b630164eed10
Adam Fahy <apfahy@gmail.com>
2020-03-26 14:56:53 -0400

    Reduces eslint errors in Pins & LocationIcon * turns-off jsx-props-no-spreading flag * moves LocationIcon to /Components * creates tracking.tsx as a place to locate tracking code * fixes fixable eslint errors in Pins

--- src/Components/Pins.tsx
+++ src/Components/Pins.tsx
@@ -1,90 +1,96 @@
 import React from 'react';
 import { Marker } from 'react-map-gl';
-import ReactGA from 'react-ga';
 import Supercluster from 'supercluster';
-import SvgLocationIcon from '../LocationIcon';
+import styled from 'styled-components';
+
+import LocationIcon from './LocationIcon';
 import Cluster from './Cluster';
+import { trackLinkClicked } from '../utils/tracking';
 
 export interface PinsProps {
-    data: any;
-    onClickPin: Function,
-    onClickCluster: Function,
-    mapRef: any;
+  data: any;
+  onClickPin: Function;
+  onClickCluster: Function;
+  mapRef: any;
 }
 
 const SIZE = 40;
 
 interface ClusterMarkerParams {
-    cluster: any;
-    superCluster: Supercluster;
-    onClickCluster: Function;
-    latitude: number;
-    longitude: number;
-    zoom: number;
+  cluster: any;
+  superCluster: Supercluster;
+  onClickCluster: Function;
+  latitude: number;
+  longitude: number;
+  zoom: number;
 }
 
-const ClusterMarker = (args: ClusterMarkerParams) => {
-  const {
-    cluster, onClickCluster, latitude, longitude, zoom,
-  } = args;
+const ClusterMarkerContainer = styled.div`
+  background: #c82c25;
+  border-radius: 50%;
+  width: 25px;
+  padding: 5px 7px;
+  text-align: center;
+  line-height: 29px;
+  color: white;
+  cursor: pointer;
+`;
+
+const ClusterMarker = (props: ClusterMarkerParams) => {
+  const { cluster, onClickCluster, latitude, longitude, zoom } = props;
   return (
-    <div
-      style={{
-        background: '#c82c25',
-        borderRadius: '50%',
-        width: '25px',
-        padding: '5px 7px',
-        textAlign: 'center',
-        lineHeight: '29px',
-        color: 'white',
-        cursor: 'pointer',
-      }}
+    <ClusterMarkerContainer
       onClick={() => {
         onClickCluster(latitude, longitude, zoom);
       }}
     >
       {cluster.properties.point_count_abbreviated}
-    </div>
+    </ClusterMarkerContainer>
   );
 };
 
 export default class Pins extends React.Component<PinsProps> {
-  handleLinkClicked(locationId: string): void {
-    ReactGA.event({
-      category: 'Location',
-      action: 'View',
-      label: locationId,
-    });
-  }
-
   shouldComponentUpdate(nextProps: Readonly<PinsProps>): boolean {
-    return nextProps.data.length !== this.props.data.length;
+    const { data } = this.props;
+    return nextProps.data.length !== data.length;
   }
 
   render() {
-    const {
-      data, onClickPin, mapRef, onClickCluster,
-    } = this.props;
+    const { data, onClickPin, mapRef, onClickCluster } = this.props;
     if (!mapRef.current || !mapRef.current.getMap()) return null;
 
     // TODO: Make a real type for the place data
     return (
-      <Cluster map={mapRef.current.getMap()} radius={32} extent={512} nodeSize={64} element={ClusterMarker} onClickCluster={onClickCluster}>
-        {data.map((place: any, index: number) => (
-          <Marker key={`marker-${index}`} longitude={place.location_longitude} latitude={place.location_latitude}>
-            <SvgLocationIcon
-              height={SIZE}
-              style={{
-                cursor: 'pointer',
-                transform: `translate(${-SIZE / 2}px,${-SIZE}px)`,
-              }}
-              onClick={() => {
-                onClickPin(place);
-                this.handleLinkClicked(place.location_id);
-              }}
-            />
-          </Marker>
-        ))}
+      <Cluster
+        map={mapRef.current.getMap()}
+        radius={32}
+        extent={512}
+        nodeSize={64}
+        element={ClusterMarker}
+        onClickCluster={onClickCluster}
+      >
+        {data.map((place: any, index: number) => {
+          /* eslint-disable react/no-array-index-key */
+          return (
+            <Marker
+              key={`marker-${index}`}
+              longitude={place.location_longitude}
+              latitude={place.location_latitude}
+            >
+              <LocationIcon
+                height={SIZE}
+                style={{
+                  cursor: 'pointer',
+                  transform: `translate(${-SIZE / 2}px,${-SIZE}px)`,
+                }}
+                onClick={() => {
+                  onClickPin(place);
+                  trackLinkClicked(place.location_id);
+                }}
+              />
+            </Marker>
+          );
+        })}
       </Cluster>
     );
   }
