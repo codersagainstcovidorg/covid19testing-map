c0fc59444b487f60e61865a321b108de958c3db4
philiparpin <philip.arpin@me.com>
2020-04-04 14:44:55 -0400

    Added logic for location testing criteria as well as additional logic for Check your Symptoms

--- src/Components/LocationModal.tsx
+++ src/Components/LocationModal.tsx
@@ -142,7 +142,8 @@ const LocationModal = ({ location, onClose }: LocationModalProps) => {
             className={classes.callToAction}
             href={
               location.location_contact_url_covid_screening_tool === '' ||
-              location.location_contact_url_covid_screening_tool === null
+              location.location_contact_url_covid_screening_tool === null ||
+              location.location_contact_url_covid_screening_tool.length < 4
                 ? 'https://www.apple.com/covid19/'
                 : location.location_contact_url_covid_screening_tool
             }
