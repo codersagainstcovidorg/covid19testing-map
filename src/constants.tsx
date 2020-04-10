7a71f59f26b8e9dfeb948adfb93863f6d2155ac1
Jorge Caballero <jorge.caballero@icloud.com>
2020-04-10 15:10:40 -0700

    updated add a location form to push data into GISCorps

--- src/constants.tsx
+++ src/constants.tsx
@@ -4,7 +4,7 @@ export const MAPBOX_TOKEN: string =
   'pk.eyJ1IjoibWFydGluYW1wcyIsImEiOiJjazd3aDNoaTQwMjNuM2ZtZTRrcm1wOHlqIn0.1c2tf5eJPHyAvC1GgO4zzg';
 
 export const ADD_LOCATION_FORM: string =
-  'https://docs.google.com/forms/d/e/1FAIpQLSe2sCuCrQwEHwi3FLiyRB9CYWRmSUiGyyK8RLsQPwhfrJTI4g/viewform';
+  'https://survey123.arcgis.com/share/ce7939ac077c4551b15bfbf3579a259c';
 
 export const GITHUB_URL: string = 'https://github.com/codersagainstcovidorg';
 export const API_URL: string =
