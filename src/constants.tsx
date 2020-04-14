ae909d8b6eb43097e17845858cbd4446215c2bb4
Jorge Caballero <jorge.caballero@icloud.com>
2020-04-14 08:15:12 -0700

    removing unused (deprecated) API KEY

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,8 +1,5 @@
 // COVID19 Mapbox public key
 // eslint-disable-next-line import/prefer-default-export
-export const MAPBOX_TOKEN: string =
-  'pk.eyJ1IjoibWFydGluYW1wcyIsImEiOiJjazd3aDNoaTQwMjNuM2ZtZTRrcm1wOHlqIn0.1c2tf5eJPHyAvC1GgO4zzg';
-
 export const ADD_LOCATION_FORM: string =
   'https://survey123.arcgis.com/share/ce7939ac077c4551b15bfbf3579a259c';
 
