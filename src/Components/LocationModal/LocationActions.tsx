8c7c81eee0dc2d1cb93f2872ebe10e40d84d6213
Jorge Caballero <jorge.caballero@icloud.com>
2020-04-13 22:27:11 -0700

    Add tooltips to location actions

--- src/Components/LocationModal/LocationActions.tsx
+++ src/Components/LocationModal/LocationActions.tsx
@@ -1,4 +1,4 @@
-import { IconButton } from '@material-ui/core';
+import { IconButton, Tooltip } from '@material-ui/core';
 import PhoneIcon from '@material-ui/icons/Phone';
 import PhoneDisabledIcon from '@material-ui/icons/PhoneDisabled';
 import DirectionsIcon from '@material-ui/icons/Directions';
@@ -15,42 +15,52 @@ const LocationActions = ({ location, onLinkClick }: LocationActionsProps) => {
     <div>
       {location.location_contact_phone_main !== null &&
       location.location_contact_phone_main.length > 4 ? (
-        <IconButton
-          area-label="call"
-          href={`tel://${location.location_contact_phone_main}`}
-          onClick={() => {
-            onLinkClick(location.location_id, 'Call');
-          }}
-        >
-          <PhoneIcon />
-        </IconButton>
+        <Tooltip title="Dial main phone number" placement="top" arrow>
+          <IconButton
+            area-label="call"
+            href={`tel://${location.location_contact_phone_main}`}
+            onClick={() => {
+              onLinkClick(location.location_id, 'Call');
+            }}
+          >
+            <PhoneIcon />
+          </IconButton>
+        </Tooltip>
       ) : (
-        <IconButton area-label="call" disabled>
-          <PhoneDisabledIcon />
-        </IconButton>
+        <Tooltip title="Main phone number unknown" placement="top" arrow>
+          <span>
+            <IconButton area-label="call" disabled>
+              <PhoneDisabledIcon />
+            </IconButton>
+          </span>
+        </Tooltip>
       )}
-      <IconButton
-        area-label="directions"
-        href={`https://www.google.com/maps/dir/?api=1&destination=${location.location_latitude},${location.location_longitude}`}
-        target="_blank"
-        rel="noopener"
-      >
-        <DirectionsIcon />
-      </IconButton>
+      <Tooltip title="Get directions" placement="top" arrow>
+        <IconButton
+          area-label="directions"
+          href={`https://www.google.com/maps/dir/?api=1&destination=${location.location_latitude},${location.location_longitude}`}
+          target="_blank"
+          rel="noopener"
+        >
+          <DirectionsIcon />
+        </IconButton>
+      </Tooltip>
       {/* <IconButton area-label="share"> */}
       {/*  <ShareIcon /> */}
       {/* </IconButton> */}
-      <IconButton
-        area-label="report"
-        href="https://docs.google.com/forms/d/e/1FAIpQLSd2xzEXfJdNJIGh5MDhxg217-p_MXvSREOuQT_P_vwrqSjEMQ/viewform?usp=sf_link"
-        target="_blank"
-        rel="noopener"
-        onClick={() => {
-          onLinkClick(location.location_id, 'Report Error');
-        }}
-      >
-        <ReportProblemIcon />
-      </IconButton>
+      <Tooltip title="Report an error" placement="top" arrow>
+        <IconButton
+          area-label="report"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSd2xzEXfJdNJIGh5MDhxg217-p_MXvSREOuQT_P_vwrqSjEMQ/viewform?usp=sf_link"
+          target="_blank"
+          rel="noopener"
+          onClick={() => {
+            onLinkClick(location.location_id, 'Report Error');
+          }}
+        >
+          <ReportProblemIcon />
+        </IconButton>
+      </Tooltip>
     </div>
   );
 };
