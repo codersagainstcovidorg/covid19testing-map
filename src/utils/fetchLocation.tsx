b6a4dd77b115c587830e64ae00329a1c890d2f54
Jorge Caballero <jorge.caballero@icloud.com>
2020-05-12 23:04:06 -0700

    enabling fetching details on click

--- src/utils/fetchLocation.tsx
+++ src/utils/fetchLocation.tsx
@@ -1,8 +1,11 @@
 const { REACT_APP_API_ENDPOINT } = process.env;
 
 const fetchLocation = async (location_id: string) => {
-    const results: Response = await fetch(`${REACT_APP_API_ENDPOINT}/api/v1/location/${location_id}`);
-    return await results.json();
+  const endpoint = `${REACT_APP_API_ENDPOINT}/api/v1/location/`;
+  const results: Response = await fetch(`${endpoint}${location_id}`);
+  const data = await results.json();
+  
+  return data
 };
 
 export default fetchLocation;
--- src/utils/fetchPins.tsx
+++ src/utils/fetchPins.tsx
@@ -3,7 +3,7 @@ import SearchFilterType from '../Components/Types/SearchFilterType';
 const { REACT_APP_API_ENDPOINT } = process.env;
 
 const fetchPins = async (searchFilters: SearchFilterType) => {
-  const endpoint = `${REACT_APP_API_ENDPOINT}/api/v1/location`;
+  const endpoint = `${REACT_APP_API_ENDPOINT}/api/v1/location?fields=location_id,location_name,location_address_locality,location_address_region,location_latitude,location_longitude,location_status,raw_data,updated_on`;
   const results: Response = await fetch(endpoint);
   const data = await results.json();
 
